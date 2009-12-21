b314c8aa640410126f5a2ab26a9af6e577d09c87
Stefan Zeiger <szeiger@novocode.com>
2009-12-21 15:24:18 +0100

    Project definition and README updated for release 0.3

--- README.txt
+++ README.txt
@@ -1,15 +1,20 @@
-An implementation of sbt's test interface <http://github.com/harrah/test-interface>
-(version 0.2) for JUnit 4.
+An implementation of sbt's test interface <http://github.com/harrah/test-interface> for JUnit 4.
+
+Unlike Scala testing frameworks like ScalaTest (which can also run JUnit test cases), both JUnit
+and this adapter are pure Java, so you can run JUnit tests with any Scala version supported by
+sbt without having to build a binary-compatible test framework first.
 
 See LICENSE.txt for licensing conditions (BSD-style).
 
-To use with xsbt 0.6.6, clone this project, publish it locally
-with "sbt update publish-local" and add the following to the definition
-of the xsbt project you want to test:
+To use with xsbt 0.6, add the following dependency to your project:
 
-  val junitInterface = "com.novocode" % "junit-interface" % "0.2"
+  val junitInterface = "com.novocode" % "junit-interface" % "0.3"
   override def testFrameworks = super.testFrameworks ++ List(new TestFramework("com.novocode.junit.JUnitFramework"))
 
-Your test classes need to implement the marker interface
-com.novocode.junit.TestMarker in order to be discovered. It is
-sufficient to add this to the test suite that bundles all test cases.
+Your test classes need to implement the marker interface com.novocode.junit.TestMarker in order
+to be discovered. It is sufficient to add this to a test suite that bundles all test cases.
+
+If you want to avoid the dependency on TestMarker, replace "JUnitFramework" above with
+"JUnitFrameworkNoMarker". This causes all top-level classes built from the test sources to be
+recognized as potential test cases. Classes with a @SuiteClasses annotation are not passed to
+JUnit to avoid running test cases twice.
--- project/build/src/JUnitInterfaceProject.scala
+++ project/build/src/JUnitInterfaceProject.scala
@@ -1,7 +1,72 @@
 import sbt._
+import scala.collection.Set
+import scala.xml._
+import java.io.{File, FileOutputStream}
+import java.nio.channels.Channels
 
 class JUnitInterfaceProject(info: ProjectInfo) extends DefaultProject(info)
 {
   val junit = "junit" % "junit" % "4.7"
   val testInterface = "org.scala-tools.testing" % "test-interface" % "0.2"
+  override def javaCompileOptions = JavaCompileOption("-target") :: JavaCompileOption("1.5") :: Nil
+
+  /*********** Publishing ***********/
+  val publishTo = Resolver.file("ScalaQuery Test Repo", new File("e:/temp/repo/"))
+  //val publishTo = "Scala Tools Snapshots" at "http://nexus.scala-tools.org/content/repositories/snapshots/"
+  //val publishTo = "Scala Tools Releases" at "http://nexus.scala-tools.org/content/repositories/releases/"
+  Credentials(Path.userHome / ".ivy2" / ".credentials", log)
+  def specificSnapshotRepo =
+    Resolver.url("scala-nightly").
+    artifacts("http://scala-tools.org/repo-snapshots/[organization]/[module]/2.8.0-SNAPSHOT/[artifact]-[revision].[ext]").
+    mavenStyle()
+  val nightlyScala = ModuleConfiguration("org.scala-lang", "*", "2.8.0-.*", specificSnapshotRepo)
+  override def deliverScalaDependencies = Nil
+  override def managedStyle = ManagedStyle.Maven
+
+  /*********** Extra meta-data for the POM ***********/
+  override def makePomAction = enrichPom dependsOn superMakePom
+  lazy val superMakePom = super.makePomAction
+  lazy val enrichPom = task {
+    val in = XML.loadFile(pomPath.asFile)
+    val out = <project xmlns="http://maven.apache.org/POM/4.0.0" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+        xsi:schemaLocation="http://maven.apache.org/POM/4.0.0 http://maven.apache.org/maven-v4_0_0.xsd">
+      {in \ "modelVersion"}
+      {in \ "groupId"}
+      {in \ "artifactId"}
+      {in \ "packaging"}
+      <name>JUnitInterface</name>
+      {in \ "version"}
+      <url>http://github.com/szeiger/junit-interface/</url>
+      <inceptionYear>2009</inceptionYear>
+      <description>An implementation of sbt's test interface for JUnit 4</description>
+      <licenses>
+        <license>
+          <name>Two-clause BSD-style license</name>
+          <url>http://github.com/szeiger/junit-interface/blob/master/LICENSE.txt</url>
+          <distribution>repo</distribution>
+        </license>
+      </licenses>
+      <developers>
+        <developer>
+          <id>szeiger</id>
+          <name>Stefan Zeiger</name>
+          <timezone>+1</timezone>
+          <email>szeiger [at] novocode.com</email>
+        </developer>
+      </developers>
+      <scm>
+        <url>http://github.com/szeiger/junit-interface/</url>
+      </scm>
+      {in \ "dependencies"}
+    </project>
+    val fos = new FileOutputStream(pomPath.asFile)
+    try {
+      val w = Channels.newWriter(fos.getChannel(), "UTF-8")
+      try {
+        w.write("<?xml version=\"1.0\" encoding=\"UTF-8\"?>\r\n")
+        w.write(new PrettyPrinter(java.lang.Integer.MAX_VALUE, 2).format(out, TopScope))
+      } finally { w.close() }
+    } finally { fos.close() }
+    None
+  }
 }
