32fd936774931a1acbdcc9ae1e6625bdfee8d13b
David Blackman <blackmad@foursquare.com>
2011-02-10 14:29:31 -0500

    Add optional flags that filter which specific testcases are being run based one or more regexps

---
+++ src/main/java/com/novocode/junit/JUnitFilter.java
@@ -0,0 +1,41 @@
+// Mostly copied from http://stackoverflow.com/questions/1230706/running-a-subset-of-junit-test-methods/1236782#1236782
+package com.novocode.junit;
+
+import java.util.regex.*;
+import org.junit.runner.manipulation.Filter;
+import org.junit.runner.Description;
+
+public final class JUnitFilter extends Filter {
+  private static final String DELIMITER = "\\,";
+
+  private String[] testPatterns;
+
+  public JUnitFilter(String testFilter) {
+    super();
+    testPatterns = testFilter.split(DELIMITER);
+  }
+
+  public String describe() {
+    return "Filters out all tests not explicitly named in a comma-delimited list in the system property 'tests'."; 
+  }
+
+  public boolean shouldRun(Description d) {
+    String displayName = d.getDisplayName();
+    
+    // We get asked both if we should run the class/suite, as well as the individual tests
+    // So let the suite always run, so we can evaluate the individual testcases
+    if (displayName.indexOf('(') == -1) {
+      return true;
+    }
+    String testName = displayName.substring(0, displayName.indexOf('('));
+    if (testPatterns == null) return true;
+
+    for (int i = 0; i < testPatterns.length; i++) {
+      if (Pattern.matches(testPatterns[i], testName)) {
+        return true;
+      }
+    }
+
+    return false;
+  }
+}
--- src/main/java/com/novocode/junit/JUnitRunner.java
+++ src/main/java/com/novocode/junit/JUnitRunner.java
@@ -1,6 +1,7 @@
 package com.novocode.junit;
 
 import org.junit.runner.JUnitCore;
+import org.junit.runner.Request;
 import org.scalatools.testing.EventHandler;
 import org.scalatools.testing.Fingerprint;
 import org.scalatools.testing.Logger;
@@ -22,10 +23,20 @@ final class JUnitRunner extends Runner2
   public void run(String testClassName, Fingerprint fingerprint, EventHandler eventHandler, String [] args)
   {
     boolean quiet = false, verbose = false;
+    String testFilter = "";
     for(String s : args)
     {
       if("-q".equals(s)) quiet = true;
       else if("-v".equals(s)) verbose = true;
+      else if(s.contains("-tests") ||
+              s.contains("-Dtests") ||
+              s.contains("-test") ||
+              s.contains("-Dtest")) {
+        String parts[] = s.split("=", 2);
+        if (parts.length == 2) {
+          testFilter = parts[1];
+        }
+      }
     }
     for(String s : args)
     {
@@ -35,10 +46,12 @@ final class JUnitRunner extends Runner2
     EventDispatcher ed = new EventDispatcher(logger, eventHandler, quiet, verbose);
     JUnitCore ju = new JUnitCore();
     ju.addListener(ed);
+
     try
     {
       Class<?> cl = testClassLoader.loadClass(testClassName);
-      try { ju.run(cl); } finally { ed.uncapture(true); }
+      Request request = Request.classes(cl).filterWith(new JUnitFilter(testFilter));
+      try { ju.run(request); } finally { ed.uncapture(true); }
     }
     catch(Exception ex) { ed.post(new TestExecutionFailedEvent(testClassName, ex)); }
   }
