af90ff934b0eb182cf9d972e29f0606896b073c8
Joakim Ohlrogge <joakim.ohlrogge@agical.com>
2011-03-19 12:19:45 +0100

    Failed assumptions should be skipped, not failed

--- src/main/java/com/novocode/junit/TestAssumptionFailedEvent.java
+++ src/main/java/com/novocode/junit/TestAssumptionFailedEvent.java
@@ -10,13 +10,13 @@ final class TestAssumptionFailedEvent extends AbstractEvent
 
   TestAssumptionFailedEvent(Failure failure)
   {
-    super(buildName(failure.getDescription()), failure.getMessage(), Result.Failure, failure.getException());
+    super(buildName(failure.getDescription()), failure.getMessage(), Result.Skipped, failure.getException());
     this.failure = failure;
   }
 
   @Override
   public void logTo(RichLogger logger)
   {
-    logger.error("Test assumption in test "+AbstractEvent.buildName(failure.getDescription())+" failed: "+failure.getMessage(), error);
+    logger.warn("Test assumption in test "+AbstractEvent.buildName(failure.getDescription())+" failed: "+failure.getMessage());
   }
 }
