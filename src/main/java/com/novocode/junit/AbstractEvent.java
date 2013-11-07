bccd2de8f9bf7711439fe97f32c324049d8040d3
Bruno Bieth <biethb@gmail.com>
2013-11-07 18:04:19 +0100

    Upgraded to the new test-interface.
    I've dropped the output capture as I'd like to move this part to SBT.
    The runner options have not been tested and might have been broken
    in the process. I'll clean this up later.

--- src/main/java/com/novocode/junit/AbstractEvent.java
+++ src/main/java/com/novocode/junit/AbstractEvent.java
@@ -1,35 +1,61 @@
 package com.novocode.junit;
 
-import org.scalatools.testing.Event;
-import org.scalatools.testing.Result;
+import sbt.testing.*;
+
 import static com.novocode.junit.Ansi.*;
 
 abstract class AbstractEvent implements Event
 {
   protected final String ansiName;
   protected final String ansiMsg;
-  protected final Result result;
+  protected final Status status;
   protected final Throwable error;
+  private final Fingerprint fingerprint;
+  private final Long duration;
 
-  AbstractEvent(String ansiName, String ansiMsg, Result result, Throwable error)
+  AbstractEvent(String ansiName, String ansiMsg, Status status, Fingerprint fingerprint, Long duration, Throwable error)
   {
+    this.fingerprint = fingerprint;
     this.ansiName = ansiName;
     this.ansiMsg = ansiMsg;
-    this.result = result;
+    this.status = status;
+    this.duration = duration;
     this.error = error;
   }
 
-  @Override
-  public final String testName() { return filterAnsi(ansiName); }
-
-  @Override
-  public final String description() { return filterAnsi(ansiMsg); }
-
-  @Override
-  public final Result result() { return result; }
-
-  @Override
-  public final Throwable error() { return error; }
-
   abstract void logTo(RichLogger logger);
+
+  @Override
+  public String fullyQualifiedName() {
+    return filterAnsi(ansiName);
+  }
+
+  @Override
+  public Fingerprint fingerprint() {
+    return fingerprint;
+  }
+
+  @Override
+  public Selector selector() {
+    return new TestSelector(fullyQualifiedName());
+  }
+
+  @Override
+  public Status status() {
+    return status;
+  }
+
+  @Override
+  public OptionalThrowable throwable() {
+    return new OptionalThrowable(error);
+  }
+
+  @Override
+  public long duration() {
+    return duration;
+  }
+
+  String durationToString() {
+    return duration / 1000.0 + " sec";
+  }
 }
